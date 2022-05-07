8ff98735039ee1cf4423ebcb420340b87a0b4d0e
Andrej <andykaczanowski@gmail.com>
2022-05-07 23:37:57 +0200

    cards section-2

--- src/components/chooseApp/ChooseAppThird.vue
+++ src/components/chooseApp/ChooseAppThird.vue
@@ -22,6 +22,27 @@
             >
           </ul>
         </div>
+        <div class="price-card">
+          <div class="upper">
+            <p class="price">$0<span>/month</span></p>
+            <h4>Business Class</h4>
+            <p>For small teams or office</p>
+          </div>
+          <ul class="ul-list">
+            <li>Drag & Drop Builder</li>
+            <li>1,000's of Templates</li>
+            <li>Blog Support Tools</li>
+            <li>eCommerce Store</li>
+            <button-test bgColor="primary">Subscribe now</button-test>
+
+            <button-test
+              bg-color="#fff"
+              btn-class="primary--text pr-8 pl-8"
+              class="trail"
+              >Or Start 14 days trail</button-test
+            >
+          </ul>
+        </div>
       </div>
     </v-row></v-container
   >
@@ -42,51 +63,69 @@ export default {
 
 .row {
   height: 50vh;
-  .price-card {
-    margin-top: 50px;
-    margin-bottom: 50px;
-    border-radius: 10px;
-    box-shadow: 0px -1px 18px -10px rgba(66, 68, 90, 1);
-  }
-  .upper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    @include set-margin-padding(0 0 0 0, 10px 10px 10px 10px);
-    background-color: #fbfcfd;
-    .price {
-      font-size: 70px;
-      font-weight: bold;
-      color: black;
-      margin: 0 auto;
 
-      span {
-        font-size: 15px;
+  .cards {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .price-card {
+      margin: 50px;
+      border-radius: 10px;
+      box-shadow: 0px -1px 18px -10px rgba(66, 68, 90, 0.3);
+      transition: box-shadow 0.3s;
+      &:hover {
+        box-shadow: 0px -1px 18px -10px rgba(66, 68, 90, 1);
+      }
+    }
+    .upper {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      @include set-margin-padding(0 0 0 0, 10px 10px 10px 10px);
+      background-color: #fbfcfd;
+      .price {
+        font-size: 70px;
+        font-weight: bold;
+        color: black;
+        margin: 0 auto;
+
+        span {
+          font-size: 15px;
+          color: #999ca5;
+        }
+      }
+      h4 {
+        font-size: 25px;
+      }
+      p {
+        font-size: 13px;
         color: #999ca5;
       }
     }
-    h4 {
-      font-size: 25px;
-    }
-    p {
-      font-size: 13px;
-      color: #999ca5;
-    }
-  }
-  .ul-list {
-    font-size: 15px;
-    list-style: none;
-    text-align: center;
-    flex-direction: column;
-    @include set-margin-padding(0 0 0 0, 30px 70px 30px 70px);
-    li {
-      margin-top: 10px;
-      margin-bottom: 20px;
-      color: #999ca5;
-    }
-    .v-btn {
-      margin: 0 !important;
+    .ul-list {
+      display: flex;
+      font-size: 15px;
+      list-style: none;
+      text-align: center;
+      flex-direction: column;
+      @include set-margin-padding(0 0 0 0, 30px 70px 30px 70px);
+      li {
+        margin-top: 10px;
+        margin-bottom: 20px;
+        color: #999ca5;
+      }
+      .v-btn {
+        margin: 0 !important;
+      }
+      .trail {
+        margin-top: 10px !important;
+        &:hover {
+          color: #fff !important;
+          background-color: $primary !important;
+        }
+      }
     }
   }
 }
